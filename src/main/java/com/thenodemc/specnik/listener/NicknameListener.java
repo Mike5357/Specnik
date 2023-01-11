475c9d8304be470a883d44710f89fc356dba5bb4
Mike <Mike5357@users.noreply.github.com>
2023-01-10 23:01:26 -0500

    Migrate nickname listener to own class

---
+++ src/main/java/com/thenodemc/specnik/listener/NicknameListener.java
@@ -0,0 +1,51 @@
+package com.thenodemc.specnik.listener;
+
+import com.pixelmonmod.api.pokemon.PokemonSpecification;
+import com.pixelmonmod.pixelmon.api.events.pokemon.SetNicknameEvent;
+import com.thenodemc.specnik.Specnik;
+import com.thenodemc.specnik.config.SpecnikConfig;
+import net.minecraft.util.Util;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class NicknameListener {
+    SpecnikConfig config = Specnik.instance.getConfig();
+
+    @SubscribeEvent
+    public void onNicknameEvent(SetNicknameEvent e) {
+        if (config.isDebug()) e.player.sendMessage(new StringTextComponent("[Debug] SetNicknameEvent Triggered! " + e.player.getName().getString() + "'s " + e.pokemon.getSpecies().getName() + " changed to: " + e.nickname), Util.NIL_UUID);
+
+        if (!e.pokemon.hasFlag("unnickable")) {
+            for (String s : config.getNicknameBlacklist()) {
+                if (config.isDebug()) e.player.sendMessage(new StringTextComponent("Checking regex: " + s), Util.NIL_UUID);
+                if (e.nickname.matches(s)) {
+                    e.player.sendMessage(new StringTextComponent(config.getLangSettings().get("nickname-blacklist-triggered").replaceAll("%nickname%", e.nickname)), Util.NIL_UUID);
+                    e.setCanceled(true);
+                    return;
+                }
+            }
+
+            for (SpecnikConfig.NicknameSetting nicknameSetting : config.getForceNicknames().values()) {
+                for (PokemonSpecification spec : nicknameSetting.getSpecsToMatch()) {
+                    if (!spec.matches(e.pokemon)) {
+                        break;
+                    }
+                    if (config.isDebug())
+                        e.player.sendMessage(new StringTextComponent("[Debug] §a✔ Pokemon matches specs: " + spec), Util.NIL_UUID);
+                    if (nicknameSetting.isPlayerEditingAllowed()) {
+                        e.nickname = config.replacePlaceholders(nicknameSetting.getName(), e.pokemon).replaceAll("%nickname%", e.nickname);
+                        if (config.isNotifyModified())
+                            e.player.sendMessage(new StringTextComponent(config.getLangSettings().get("notify-modified-message").replaceAll("%nickname%", e.nickname)), Util.NIL_UUID);
+                    } else {
+                        e.player.sendMessage(new StringTextComponent(config.getLangSettings().get("editing-not-allowed-message")), Util.NIL_UUID);
+                        e.setCanceled(true);
+                    }
+                    return;
+                }
+            }
+        } else {
+            e.player.sendMessage(new StringTextComponent(config.getLangSettings().get("editing-not-allowed-message")), Util.NIL_UUID);
+            e.setCanceled(true);
+        }
+    }
+}
