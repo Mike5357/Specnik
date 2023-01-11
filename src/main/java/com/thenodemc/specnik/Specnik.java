5ab2dfff2d67276652bef792c5f33690b8c95bed
Mike <Mike5357@users.noreply.github.com>
2023-01-10 23:01:49 -0500

    Migrate nickname listener to own class

--- src/main/java/com/thenodemc/specnik/Specnik.java
+++ src/main/java/com/thenodemc/specnik/Specnik.java
@@ -1,13 +1,11 @@
 package com.thenodemc.specnik;
 
-import com.pixelmonmod.api.pokemon.PokemonSpecification;
 import com.pixelmonmod.api.pokemon.PokemonSpecificationProxy;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.api.config.api.yaml.YamlConfigFactory;
-import com.pixelmonmod.pixelmon.api.events.pokemon.SetNicknameEvent;
-
-import net.minecraft.util.Util;
-import net.minecraft.util.text.StringTextComponent;
+import com.thenodemc.specnik.command.SpecnikCommand;
+import com.thenodemc.specnik.config.SpecnikConfig;
+import com.thenodemc.specnik.spec.UnnickableRequirement;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -65,44 +63,6 @@ public class Specnik {
 
     public SpecnikConfig getConfig() {
         return this.config;
-    }
-
-    @SubscribeEvent
-    public void onNicknameEvent(SetNicknameEvent e) {
-        if (config.isDebug()) e.player.sendMessage(new StringTextComponent("[Debug] SetNicknameEvent Triggered! " + e.player.getName().getString() + "'s " + e.pokemon.getSpecies().getName() + " changed to: " + e.nickname), Util.NIL_UUID);
-
-        if (!e.pokemon.hasFlag("unnickable")) {
-            for (String s : config.getNicknameBlacklist()) {
-                if (config.isDebug()) e.player.sendMessage(new StringTextComponent("Checking regex: " + s), Util.NIL_UUID);
-                if (e.nickname.matches(s)) {
-                    e.player.sendMessage(new StringTextComponent(config.getLangSettings().get("nickname-blacklist-triggered").replaceAll("%nickname%", e.nickname)), Util.NIL_UUID);
-                    e.setCanceled(true);
-                    return;
-                }
-            }
-
-            for (SpecnikConfig.NicknameSetting nicknameSetting : config.getForceNicknames().values()) {
-                for (PokemonSpecification spec : nicknameSetting.getSpecsToMatch()) {
-                    if (!spec.matches(e.pokemon)) {
-                        break;
-                    }
-                    if (config.isDebug())
-                        e.player.sendMessage(new StringTextComponent("[Debug] §a✔ Pokemon matches specs: " + spec), Util.NIL_UUID);
-                    if (nicknameSetting.isPlayerEditingAllowed()) {
-                        e.nickname = config.replacePlaceholders(nicknameSetting.getName(), e.pokemon).replaceAll("%nickname%", e.nickname);
-                        if (config.isNotifyModified())
-                            e.player.sendMessage(new StringTextComponent(config.getLangSettings().get("notify-modified-message").replaceAll("%nickname%", e.nickname)), Util.NIL_UUID);
-                    } else {
-                        e.player.sendMessage(new StringTextComponent(config.getLangSettings().get("editing-not-allowed-message")), Util.NIL_UUID);
-                        e.setCanceled(true);
-                    }
-                    return;
-                }
-            }
-        } else {
-            e.player.sendMessage(new StringTextComponent(config.getLangSettings().get("editing-not-allowed-message")), Util.NIL_UUID);
-            e.setCanceled(true);
-        }
     }
 
 }
